49bf3e3fcd3d2f549208a48e34f619c6beb3ddef
Leonardo Fioretti <leeofmt@gmail.com>
2023-10-28 01:04:36 -0300

    Incluindo autenticação

--- src/database-postgres.js
+++ src/database-postgres.js
@@ -1,10 +1,56 @@
 import { randomUUID } from "node:crypto";
 import sql from "./db.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import 'dotenv/config';
 
 export class DatabaseProgress{
-    async list(search){
-        
+    async verifyToken(token){
+        const decodedToken = jwt.verify(token, process.env.JWTTOKEN_DEV);
+        const {userId} = decodedToken;
+        return userId;
     }
+    
+    async list(userId){
+        console.log(userId);
+        const retSQL = await sql`SELECT NOME, ID_USER FROM USUARIOS WHERE ID_USER = ${userId}`;
+        return {
+            status: "200", 
+            message: retSQL
+        };
+    }
+    async auth(authObject){
+        const user = authObject.usuario;
+        const password = authObject.password;
+        const retSQL = await sql`SELECT ID_USER, SENHA FROM USUARIOS WHERE USUARIO = ${user}`;
+
+        if(retSQL.length > 0){
+            const senhaBanco = retSQL[0].senha;
+
+            const isSamePassword = bcrypt.compareSync(password, senhaBanco);
+            if(!isSamePassword){
+                return {
+                    status: "401", 
+                    message: "Senha não confere"
+                };
+            }
+
+            const userId = retSQL[0].id_user;
+            const token = jwt.sign({userId}, process.env.JWTTOKEN_DEV, {expiresIn: "1d"});
+
+            return {
+                status: "201", 
+                message: `Autenticação válida. Token: ${token}`
+            };
+        }
+        else{
+            return {
+                status: "401", 
+                message: "Usuário não identificado"
+            };
+        }        
+    }
+
     async create(user){
         const userId = await randomUUID();
         const { usuario, nome, senha, email, nascimento, psn, xbox, steam } = user;
@@ -13,8 +59,10 @@ export class DatabaseProgress{
             if( !usuario || !nome || !senha || !email || !nascimento){
                 throw "Campo obrigatório não preenchido";
             }
-
-            await sql`insert into USUARIOS (ID_USER, USUARIO, NOME, SENHA, EMAIL, DATA_NASCIMENTO, ID_PSN, ID_XBOX, ID_STEAM) values (${userId}, ${usuario}, ${nome}, ${senha}, ${email}, ${nascimento}, ${psn??''}, ${xbox??''}, ${steam??''})`;
+            console.log(`Senha antes da criptografia: ${senha}`);
+            const newSenha = bcrypt.hashSync(senha, 10);
+            console.log(`Senha depois da criptografia: ${newSenha}`);
+            await sql`insert into USUARIOS (ID_USER, USUARIO, NOME, SENHA, EMAIL, DATA_NASCIMENTO, ID_PSN, ID_XBOX, ID_STEAM) values (${userId}, ${usuario}, ${nome}, ${newSenha}, ${email}, ${nascimento}, ${psn??''}, ${xbox??''}, ${steam??''})`;
 
             return {
                 status: "201",
--- src/server.js
+++ src/server.js
@@ -4,21 +4,58 @@ const server = fastify();
 
 const database = new DatabaseProgress();
 
+//Executar autenticação
+server.post('/auth', async (request, reply) => {
+    const body = request.body;
+    const ret = await database.auth( body );
+    if(ret.status == "200"){
+        return reply.status(200).send( ret );
+    }
+    else{
+        return reply.status(401).send( ret );
+    }
+    
+});
+
+
 //Retornar usuário
-server.get('/users/:id', function(request, reply){
+server.get('/users/:id', async (request, reply) => {
     const userId = request.params.id;
-    database.list( userId );
-
-
-
+    console.log("userId consulta");
+    const ret = await database.list( userId );
+    console.log(ret);
     return reply.status(200).send( ret );
 });
 
-//Incluir Usuários
-server.post('/users', async function(request, reply){
-    const body = request.body;
-    console.log(body);
+//Retornar rota autenticada
+server.get(
+    '/testAuthorization',
+    {
+        preHandler: async (request, reply, done) => {
+            //Bearer token
+            const token = request.headers.authorization?.replace(/^Bearer /, "");
+            if(!token){
+                return reply.status(401).send({message: `Unauthorized: token missing`});
+            }
 
+            const userId = await database.verifyToken(token);
+            if(!userId){
+                reply.status(404).send({message: "Usuário não identificado!"});
+            }
+            request.auth = {userId};
+
+            done();
+        }
+    },
+    async (request, reply) => {
+        const userId = request.auth.userId;
+        return reply.status(200).send( userId );
+    }
+);
+
+//Incluir Usuários
+server.post('/users', async (request, reply) =>{
+    const body = request.body;
     const ret = await database.create(body);
     if(ret.status == "401"){
         return reply.status(401).send(ret.message);
@@ -28,7 +65,7 @@ server.post('/users', async function(request, reply){
 });
 
 //Atualizar usuários
-server.put('/users', function(request, reply){
+server.put('/users', (request, reply) =>{
     return reply.status(204).send();
 });
 
